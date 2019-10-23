99db517b6bcc6be213764f95f2b7a4920d32ab1f
unknown <you@example.com>
2019-10-24 02:45:13 +0600

    Question view with slug

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -51,9 +51,10 @@ class QuestionsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Question $question)
     {
-        //
+        $question->increment('views');
+        return view('questions.show', compact('question'));
     }
 
     /**
