45d7337fe89ce7653956383d186987fdb16032b6
unknown <you@example.com>
2019-10-24 00:33:24 +0600

    Display all question with status

---
+++ app/Question.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+class Question extends Model
+{
+    protected $fillable = ['title', 'body'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->id);
+    }
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        if( $this->answers > 0){
+            if($this->best_answere_id){
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
+}
--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function getUrlAttribure()
+    {
+        return "#";//return route("question.show", $this->id);
+    }
 }
