6f713cc056514d822b638f582271299a5033919d
unknown <you@example.com>
2019-11-08 01:23:13 +0600

    Answer delete functionality

--- app/Answer.php
+++ app/Answer.php
@@ -33,7 +33,9 @@ class Answer extends Model
         parent::boot();
         static::created(function($answer){
             $answer->question->increment('answers_count');
-            $answer->question->save();
+        });
+        static::deleted(function($answer){
+            $answer->question->decrement('answers_count');
         });
 
     }
